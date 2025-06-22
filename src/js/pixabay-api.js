0f9b1a128f9c98f6e8829b893bf5c1e6e4e040d1
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-06-22 14:24:04 +0300

    update enviroment

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-require('dotenv').config();
 
 const searchParams = {
-  key: process.env.API_KEY,
+  key: import.meta.env.VITE_API_KEY,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
